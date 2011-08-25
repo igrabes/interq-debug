17291302afdb5fcc6bcf46fe68d0bc5af97f3622
Ian <ian.grabill@gmail.com>
2011-08-24 22:21:07 -0400

    adding admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110822044003) do
+ActiveRecord::Schema.define(:version => 20110825021537) do
 
   create_table "answers", :force => true do |t|
     t.integer  "job_id"
